54dbac8c29c9db75823726d0fd1a09647185c302
Jovit-Mathew236 <70875875+Jovit-Mathew236@users.noreply.github.com>
2020-10-07 23:28:37 +0530

    Add files via upload

--- java.js
+++ java.js
@@ -4,9 +4,22 @@ function myClick(){
 
 
 
-/*function changeTheme() {
-   document.body.style.backgroundImage = "url('tew1.jpg')";
+function changeTheme() {
+  /* var one = document.getElementById("body").style.backgroundImage = "url('bg1.jpg')";
+   var two = document.getElementById("body").style.backgroundImage = "url('bg1.jpg')";
+   var three = document.getElementById("body").style.backgroundImage = "url('bg2.jpg')";
+   */
+   var tt = 1
+   var tu = 1
+  
 
+   if (tt === tu ) {
+       document.getElementById("body").style.backgroundImage = "url('bg2.jpg')";
+       tt = tt+1;
+   } else {
+       document.getElementById("body").style.backgroundImage = "url('bg1.jpg')";
+       tt = tt-1;
+   }
 }
 
   
