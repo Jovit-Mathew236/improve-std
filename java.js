9a109b334973c89d524f8257d29a0a1781de1f55
Jovit-Mathew236 <70875875+Jovit-Mathew236@users.noreply.github.com>
2020-10-09 17:32:58 +0530

    Add files via upload

--- java.js
+++ java.js
@@ -4,17 +4,35 @@ function myClick(){
 
 
 
-function changeTheme() {
-   var bgimg = document.getElementById("body").style.backgroundImage = "url('bg1.jpg')";
-   
-   
+function changeTheme2() {
+   var currentColor2 = document.getElementById("body2").style.backgroundColor;
  
-   if (bgimg != document.getElementById("body").style.backgroundImage) {
-        document.getElementById("body").style.backgroundImage = "url('bg2.jpg')";
-   } 
-   if(bgimg == document.getElementById("body").style.backgroundImage){
-        document.getElementById("body").style.backgroundImage = "url('bg1.jpg')"
+   
+      if (currentColor2 == "whitesmoke") {
+        document.getElementById("body2").style.backgroundColor = "black";
+   } else {
+        document.getElementById("body2").style.backgroundColor = "whitesmoke";
    }
 }
 
-  
+function changeTheme3() {
+     var currentColor2 = document.getElementById("body3").style.backgroundColor;
+   
+     
+        if (currentColor2 == "whitesmoke") {
+          document.getElementById("body3").style.backgroundColor = "black";
+     } else {
+          document.getElementById("body3").style.backgroundColor = "whitesmoke";
+     }
+  }
+
+function changeTheme4() {
+     var currentColor2 = document.getElementById("body4").style.backgroundColor;
+   
+     
+        if (currentColor2 == "whitesmoke") {
+          document.getElementById("body4").style.backgroundColor = "black";
+     } else {
+          document.getElementById("body4").style.backgroundColor = "whitesmoke";
+     }
+  }
