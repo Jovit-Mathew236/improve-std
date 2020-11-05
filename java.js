5674faff4523fbda0e8b9c59ea8e30c0189c4417
Jovit-Mathew236 <70875875+Jovit-Mathew236@users.noreply.github.com>
2020-11-05 09:54:37 -0800

    Add files via upload

--- java.js
+++ java.js
@@ -69,14 +69,19 @@ $(document).ready(function () {
 function dim1Change() {
      var str = document.getElementById("1Dim").innerHTML;
      var str1 = document.getElementById("Dim").innerHTML;
-     if (str.innerHTML = "three"){
+     var str2 = document.getElementById("obj").innerHTML;
+     if (str.innerHTML = "three") {
           var res = str.replace("three", "two");
      }
-     if (str1.innerHTML = "three"){
+     if (str1.innerHTML = "three") {
           var res1 = str1.replace("three", "two");
+     }
+     if (str2.innerHTML = "space") {
+          var res2 = str2.replace("space", "palne");
      }
      document.getElementById("1Dim").innerHTML = res;
      document.getElementById("Dim").innerHTML = res1;
+     document.getElementById("obj").innerHTML = res2;
      document.getElementById("dim1").style.display = "none";
      document.getElementById("dim4").style.display = "block";
      document.getElementById("dim2").style.display = "block";
@@ -85,38 +90,49 @@ function dim1Change() {
 function dim2Change() {
      var str = document.getElementById("1Dim").innerHTML;
      var str1 = document.getElementById("Dim").innerHTML;
+     var str2 = document.getElementById("obj").innerHTML;
      var res = str.replace("one", "two");
      var res1 = str1.replace("one", "two");
+     var res2 = str2.replace("straight line", "plane");
      document.getElementById("dim2").style.display = "none";
      document.getElementById("dim3").style.display = "block";
      document.getElementById("dim1").style.display = "none";
      document.getElementById("dim4").style.display = "block";
      document.getElementById("1Dim").innerHTML = res;
      document.getElementById("Dim").innerHTML = res1;
+     document.getElementById("obj").innerHTML = res2;
 }
 function dim3Change() {
      var str = document.getElementById("1Dim").innerHTML;
      var str1 = document.getElementById("Dim").innerHTML;
+     var str2 = document.getElementById("obj").innerHTML;
      var res = str.replace("two", "three");
      var res1 = str1.replace("two", "three");
+     var res2 = str2.replace("plane", "space");
      document.getElementById("dim3").style.display = "none";
      document.getElementById("dim2").style.display = "block";
      document.getElementById("dim1").style.display = "block";
      document.getElementById("dim4").style.display = "none";
      document.getElementById("1Dim").innerHTML = res;
      document.getElementById("Dim").innerHTML = res1;
+     document.getElementById("obj").innerHTML = res2;
 }
 function dim4Change() {
      var str = document.getElementById("1Dim").innerHTML;
      var str1 = document.getElementById("Dim").innerHTML;
+     var str2 = document.getElementById("obj").innerHTML;
      if (str.innerHTML = "two") {
           var res = str.replace("two", "one");
      }
      if (str1.innerHTML = "two") {
           var res1 = str1.replace("two", "one");
      }
+     if (str2.innerHTML = "plane") {
+          var res2 = str2.replace("plane", "straight line");
+     }
      document.getElementById("1Dim").innerHTML = res;
      document.getElementById("Dim").innerHTML = res1;
+     document.getElementById("obj").innerHTML = res2;
      document.getElementById("dim4").style.display = "none";
      document.getElementById("dim1").style.display = "block";
      document.getElementById("dim2").style.display = "block";
